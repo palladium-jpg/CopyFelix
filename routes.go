7486a7d23b4cf12ec1d94287d0b679271185ddba
palladium-jpg <1589396476@qq.com>
2022-04-13 15:03:28 +0800

    用户登录

--- routes.go
+++ routes.go
@@ -7,7 +7,8 @@ import (
 
 func CollectRoute(r *gin.Engine) *gin.Engine {
 
-	r.POST("/bing", controller.Register)
+	r.POST("/api/auth/register", controller.Register)
+	r.POST("api/auth/login", controller.Login)
 	return r
 
 }
